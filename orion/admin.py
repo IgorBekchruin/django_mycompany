499901facda119b06832858c1010d7bbbb9f81ff
IgorBekchruin <worki.iba@gmail.com>
2023-11-09 12:14:55 +0400

    send mail from contact form

--- orion/admin.py
+++ orion/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import Message
-
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'message',)
-    list_display_links = ('name',)
-
-
--- orion/models.py
+++ orion/models.py
@@ -1,15 +1,1 @@
 from django.db import models
-from django.core.validators import RegexValidator
-
-
-class Message(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    message = models.TextField()
-
-    def __str__(self):
-        return self.email
-
-    class Meta:
-        verbose_name = 'Сообщение'
-        verbose_name_plural = 'Сообщения'
